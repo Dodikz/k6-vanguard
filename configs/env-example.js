bce6ba2fce560f655742aac264c7a62d137b0fd7
Dodik Vexel <sabdoilahidodik735@gmail.com>
2026-01-09 09:18:08 +0700

    env configuration

---
+++ configs/env-example.js
@@ -0,0 +1,8 @@
+export const env = {
+  BASE_URL: 'https://test.k6.io',
+  API_URL: 'https://test-api.k6.io',
+  GRAPHQL_URL: 'https://test-api.k6.io/graphql',
+  WS_URL: 'wss://test.k6.io/ws',
+  GRPC_URL: 'localhost:10000',
+  AUTH_TOKEN: 'public_test_token_12345'
+};
